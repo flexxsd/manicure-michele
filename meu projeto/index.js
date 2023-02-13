e007a0a72a96e94c5e97e948f531c25ff60b3e99
flexxsd <125307849+flexxsd@users.noreply.github.com>
2023-02-13 16:38:45 -0300

    Add files via upload

---
+++ meu projeto/index.js
@@ -0,0 +1,6 @@
+const instagramButton = document.getElementById('instagram-button');
+instagramButton.addEventListener('click', function() {
+  window.location.href = "https://www.instagram.com";
+});
+
+
